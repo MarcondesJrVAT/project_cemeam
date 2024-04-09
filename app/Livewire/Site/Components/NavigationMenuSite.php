8ff2dee8e2dfc0f1bb35e41e7a56e521f0eff2e3
MarcondesJrVAT <marcondes.junior@vat.com.br>
2024-04-09 09:42:34 -0400

    Componente NavigationMenu Site

---
+++ app/Livewire/Site/Components/NavigationMenuSite.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Site\Components;
+
+use Livewire\Component;
+
+class NavigationMenuSite extends Component
+{
+    public function render()
+    {
+        return view('livewire.site.components.navigation-menu-site');
+    }
+}
