9a464c9389164dabbb4ce4ba19c2c2ba0cea9c73
MarcondesJrVAT <marcondes.junior@vat.com.br>
2024-04-09 09:41:52 -0400

    Componente Footer Site

---
+++ app/Livewire/Site/Components/FooterSite.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Site\Components;
+
+use Livewire\Component;
+
+class FooterSite extends Component
+{
+    public function render()
+    {
+        return view('livewire.site.components.footer-site');
+    }
+}
