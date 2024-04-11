151c7c349d581d827a20a579ffccd8df218135d9
MarcondesJrVAT <marcondes.junior@vat.com.br>
2024-04-11 16:09:15 -0400

    Model de Course com relacionamento entre Details, Years, Categories, Courses, Subjects, Grades e Lessons

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,8 @@ use App\Traits\HasRoles;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -72,8 +74,43 @@ class User extends Authenticatable
         return $this->belongsToMany(Role::class);
     }
 
-    public function permissions(): Collection
+    public function permissions()
     {
-        return $this->roles->map->permissions->flatten()->pluck('slug')->unique();
+        return $this->roles->map->permissions->flatten()->pluck('slug');
+    }
+
+    public function details(): HasOne
+    {
+        return $this->hasOne(Details::class);
+    }
+
+    public function years(): HasMany
+    {
+        return $this->hasMany(Year::class, 'author_id');
+    }
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany(Category::class, 'author_id');
+    }
+
+    public function courses(): HasMany
+    {
+        return $this->hasMany(Course::class, 'author_id');
+    }
+
+    public function subjects(): HasMany
+    {
+        return $this->hasMany(Subject::class, 'author_id');
+    }
+
+    public function grades(): HasMany
+    {
+        return $this->hasMany(Grade::class, 'author_id');
+    }
+
+    public function lessons(): BelongsToMany
+    {
+        return $this->belongsToMany(Lesson::class, 'lesson_user', 'teacher_id', 'lesson_id');
     }
 }
