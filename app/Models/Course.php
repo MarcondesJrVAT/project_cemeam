7d8d6ed5498e29456e083d5b5875b08babdaf522
MarcondesJrVAT <marcondes.junior@vat.com.br>
2024-04-12 13:32:44 -0400

    Implementação de Lesson atraves do model Grade

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class Course extends Model
 {
@@ -37,5 +38,10 @@ class Course extends Model
     public function grades(): HasMany
     {
         return $this->hasMany(Grade::class);
+    }
+
+    public function lessons(): HasManyThrough
+    {
+        return $this->hasManyThrough(Lesson::class, Grade::class);
     }
 }
