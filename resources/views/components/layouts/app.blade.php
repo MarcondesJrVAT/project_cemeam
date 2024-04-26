fb6cf1d1b5131609f7d761e255e6bb8318e3cae3
MarcondesJrVAT <marcondes.junior@vat.com.br>
2024-04-26 08:42:57 -0400

    Criação de novo layout

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,47 @@
+@php use Illuminate\Support\Facades\Auth; @endphp
+        <!DOCTYPE html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" x-data="{ darkMode: false }"
+      x-bind:class="{'dark' : darkMode === true}" x-init="
+    if (!('darkMode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        localStorage.setItem('darkMode', JSON.stringify(true));
+    }
+    darkMode = JSON.parse(localStorage.getItem('darkMode'));
+    $watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))"
+>
+
+<head>
+    <meta charset="utf-8">
+    <title>{{ $title ?? 'Laravel Livewire Web App' }}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- # Main Style Sheet -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{ asset('/front/css/style.css') }}">
+    @livewireStyles
+    @stack('styles')
+</head>
+
+<body class="font-sans antialiased">
+<div class="min-h-full bg-gray-100 dark:bg-gray-900">
+    <!-- navigation -->
+    @livewire('site.components.navigation-menu-site')
+    <!-- /navigation -->
+
+    {{ $slot }}
+</div>
+<!-- footer -->
+@livewire('site.components.footer-site')
+<!-- /footer -->
+
+<!-- Main Script -->
+@livewireScripts
+@stack('scripts')
+@livewire('components.dark-mode')
+</body>
+</html>
