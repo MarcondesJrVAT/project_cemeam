f56d64fd9af553b2dd17747234810461fd988401
MarcondesJrVAT <marcondes.junior@vat.com.br>
2024-04-26 08:42:30 -0400

    Inclusão de badges na tela inicial

--- resources/views/admin/principal/dashboard.blade.php
+++ resources/views/admin/principal/dashboard.blade.php
@@ -8,7 +8,23 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+                <ul class="flex flex-col md:grid grid-cols-3 gap-5 text-redis-neutral-800 max-w-2xl mx-auto p-10 mt-10">
+                    <li
+                        class="w-full text-sm font-semibold text-slate-900 p-6 bg-white border border-slate-900/10 bg-clip-padding shadow-md shadow-slate-900/5 rounded-lg flex flex-col justify-center">
+                        <span class="mb-1 text-teal-400 font-display text-5xl">45K+</span>
+                        Usuários
+                    </li>
+                    <li
+                        class="w-full text-sm font-semibold text-slate-900 p-6 bg-white border border-slate-900/10 bg-clip-padding shadow-md shadow-slate-900/5 rounded-lg flex flex-col justify-center">
+                        <span class="mb-1 text-teal-400 font-display text-5xl">78K+</span>
+                        Downloads
+                    </li>
+                    <li
+                        class="w-full text-sm font-semibold text-slate-900 p-6 bg-white border border-slate-900/10 bg-clip-padding shadow-md shadow-slate-900/5 rounded-lg flex flex-col justify-center">
+                        <span class="mb-1 text-teal-400 font-display text-5xl">50+</span>
+                        Products
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
